55b2dfd94e19b9bc48b7f79d98b5f029102690fe
Kleriz171 <bibakleris@gmail.com>
2025-10-30 21:31:17 +0100

    dasddfsdf# Git commit message

--- src/home.jsx
+++ src/home.jsx
@@ -1,15 +1,21 @@
+import { use, useState } from "react";
+
 function Home() {
+
+    const [name, setName] = useState("mario")
+    const [age, setAge] = useState(25)
+    
+
     const handleClick = () => {
-        console.log();
+        setName("luigi");
+        console.log("clicked button");
+        setAge(30);
     }
-    const handleClick2 = (name) => {
-        console.log('Hello ' + name);
-    };
     return (
         <div>
             <h2>Welcome to the Home Page</h2>
+            <h1>{name} {age}</h1>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() => handleClick2('Amina')}>Amina</button>
         </div>
     );
 }
